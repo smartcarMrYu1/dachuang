777735019a6d77274de6fbde287183c73419d94b
smartcarMrYu1 <mingtianzhichu@126.com>
2018-09-04 17:44:18 +0800

    [规范]  将引脚号定义成宏，避免代码中出现数字

--- applications/HC-SR501.c
+++ applications/HC-SR501.c
@@ -12,6 +12,7 @@
 #include <rtdevice.h>
 
 #define EVENT_W_PIN  (0x01)
+#define HCSR_PIN     (4)
 
 static rt_mq_t hcsr_mq;
 static rt_timer_t hcs_timer;
@@ -27,26 +28,25 @@ static void HCSR501_timer_callback(void *parameter)   //回调函数尽量的简短，起到
      }
 }
 
-
 static void HCSR501_thread_entry(void *parameter)
 {
     rt_uint8_t HCSR501_data;
     rt_uint8_t pin_value;
-    rt_pin_mode(77,PIN_MODE_INPUT);
+    rt_pin_mode(HCSR_PIN,PIN_MODE_INPUT);
     while (1)
     {
         if (rt_mq_recv(hcsr_mq, &HCSR501_data, sizeof(HCSR501_data), RT_WAITING_FOREVER) == RT_EOK)
         {
-         switch(HCSR501_data)
-         {
-             case EVENT_W_PIN:
-             {
-                pin_value = rt_pin_read(126);   
-                 rt_kprintf("pin_value:%d",&pin_value);
-             }
-         }
+            switch(HCSR501_data)
+            {
+            case EVENT_W_PIN:
+            {
+                pin_value = rt_pin_read(HCSR_PIN);   
+                rt_kprintf("pin_value:%d",&pin_value);
+            }
+            }
         }
-//        rt_thread_delay(10);
+    //        rt_thread_delay(10);
     }
 }
 
@@ -54,14 +54,14 @@ int HCSR501_part_init(void)
 {
 	rt_thread_t tid;
 
-	hcsr_mq = rt_mq_create("hcsr_mq",32,2,RT_IPC_FLAG_FIFO);
+	hcsr_mq = rt_mq_create("hcsr_mq",16,2,RT_IPC_FLAG_FIFO);
     if(hcsr_mq == RT_NULL)
     {
         rt_kprintf("F:%s L:%d err! mq create fail!\n,",__FUNCTION__,__LINE__);
         return -1;
     }
 
-    hcs_timer = rt_timer_create("HCSR501_timer",HCSR501_timer_callback,RT_NULL,1000,RT_TIMER_FLAG_PERIODIC);
+    hcs_timer = rt_timer_create("HCSR501_timer",HCSR501_timer_callback,RT_NULL,100,RT_TIMER_FLAG_PERIODIC);
     if(hcs_timer == RT_NULL)
     {
         rt_kprintf("F:%s L:%d err! hcs_timer create fail!\n,",__FUNCTION__,__LINE__);
@@ -80,4 +80,11 @@ int HCSR501_part_init(void)
      
      rt_thread_startup(tid);
      rt_timer_start(hcs_timer);     
+     return 0;
 }
+
+
+/*总结一般步骤：
+  1、首先把需要的线程、定时器、消息队列创建好
+  2、如果创建失败，则删除，以释放资源
+*/
