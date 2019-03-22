81577516ccf252b9d365d5cd77850d5472d2957c
smartcarMrYu1 <mingtianzhichu@126.com>
2019-03-22 18:26:04 +0800

    [增加] 增加public_def.h,并增加电联led小功能。

--- applications/public_def.h
+++ applications/public_def.h
@@ -9,16 +9,16 @@
 #define LED_1  21
 #define LED_2  22
 
-void led_light(void)
+void led1_light(void)
 {
     rt_pin_mode(LED_1,PIN_MODE_OUTPUT);
-	  rt_pin_write(LED_1,1);
-		
-	  rt_thread_delay(100);
-	  
+	  rt_pin_write(LED_1,0);
+}
+
+void led2_light(void)
+{
 		rt_pin_mode(LED_2,PIN_MODE_OUTPUT);
 	  rt_pin_write(LED_2,0);
-	
 }
 
 #endif
